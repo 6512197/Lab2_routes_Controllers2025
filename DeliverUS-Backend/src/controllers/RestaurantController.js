e8938d43b2d883234222af360fc71111eed5fc18
Ayah Dahmani <ayadah@alumn.us.es>
2025-02-26 22:47:30 +0100

    controler of RestaurtCategory expalined

--- DeliverUS-Backend/src/controllers/RestaurantController.js
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -22,7 +22,17 @@ const index = async function (req, res) {
 // TODO: Complete the following functions
 
 const create = async function (req, res) {
+  // creating new elements, the Model.build method that receives a JSON object 
 
+  const newRestaurant = Restaurant.build(req.body)
+  // newRestaurant.userId = req.user.id authentificated
+  newRestaurant.userId = 1
+  try {
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const show = async function (req, res) {
